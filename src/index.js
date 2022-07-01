01ed2bcd700f935a2b28b8cbf7395415a46c01ae
zbl14 <ifthereisoneday@gmail.com>
2022-07-01 15:20:32 -0700

    apply session storage for the rate

--- src/index.js
+++ src/index.js
@@ -14,31 +14,40 @@ let exchangeList = (response) => {
     }
     $('#fromCurrency').append(currencylist);
     $('#toCurrency').append(currencylist);
+    const ratesStr = JSON.stringify(response.conversion_rates);
+    sessionStorage.setItem("rate", ratesStr);
   } else {
     $('.showErrors').show().text(`There was an error processing your request: ${response}`);
   }
 };
 
-let exchange = (response, toCurrency, amount) => {
-  if (response.conversion_rates) {
-    let toCurrencyAmount = `${response.conversion_rates[toCurrency]}` * amount;
-    $(".fromCurrencyAmount").val(`${amount} ${response.base_code}`);
-    $(".toCurrencyAmount").val(`${toCurrencyAmount} ${toCurrency}`);
-  } else if (response["error-type"]) {
-    $('.showErrors').show().text(`${response["error-type"]}. The currency in question doesn't exist.`);
-  } else {
-    $('.showErrors').show().text(`There was an error processing your request: ${response}`);
-  }
-};
+// let exchange = (response, toCurrency, amount) => {
+//   if (response.conversion_rates) {
+//     let toCurrencyAmount = `${response.conversion_rates[toCurrency]}` * amount;
+//     $(".fromCurrencyAmount").val(`${amount} ${response.base_code}`);
+//     $(".toCurrencyAmount").val(`${toCurrencyAmount} ${toCurrency}`);
+//   } else if (response["error-type"]) {
+//     $('.showErrors').show().text(`${response["error-type"]}. The currency in question doesn't exist.`);
+//   } else {
+//     $('.showErrors').show().text(`There was an error processing your request: ${response}`);
+//   }
+// };
 
 async function makeApiCallList(query) {
   const response = await ExchangeService.getExchangeRate(query);
   exchangeList (response);
 }
 
-async function makeApiCall(fromCurrency, toCurrency, amount) {
-  const response = await ExchangeService.getExchangeRate(fromCurrency);
-  exchange(response, toCurrency, amount);
+// async function makeApiCall(fromCurrency, toCurrency, amount) {
+//   const response = await ExchangeService.getExchangeRate(fromCurrency);
+//   exchange(response, toCurrency, amount);
+// }
+
+let exchange = (fromCurrency, toCurrency, amount) => {
+  let data = JSON.parse(sessionStorage.rate);
+  let x = amount / data[fromCurrency] * data[toCurrency]
+  $(".fromCurrencyAmount").val(`${amount} ${fromCurrency}`);
+  $(".toCurrencyAmount").val(x);
 }
 
 $(document).ready(function() {
@@ -47,6 +56,7 @@ $(document).ready(function() {
     let fromCurrency = $('#fromCurrency').val();
     let toCurrency = $('#toCurrency').val();
     let amount = $('#amount').val();
-    makeApiCall(fromCurrency, toCurrency, amount);
+    exchange(fromCurrency, toCurrency, amount)
+    // makeApiCall(fromCurrency, toCurrency, amount);
   });
 }); 
