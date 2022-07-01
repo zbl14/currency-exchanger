efba0a2bc0ca60ae2ef6ee74c8af368b36c25335
zbl14 <ifthereisoneday@gmail.com>
2022-07-01 09:26:58 -0700

    async function for geting exchange rate

--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ import $ from 'jquery';
 import { ExchangeService } from './services/exchange-service.js';
 
 let exchange = (from, to) => {
-  let to = to
+  let to = to;
   if (from.conversion_rates) {
-    $(".toCurrency").val(`${from.conversion_rates.to}`)
+    $(".toCurrency").val(`${from.conversion_rates.to}`);
+  } else {
+    $('.showErrors').text(`There was an error processing your request: ${response}`);
   }
 }
+
+async function makeApiCall(from, to) {
+  const response = await ExchangeService.getExchangeRate(from);
+  exchange(response, to);
+}
+
+$(document).ready(()=> {
+  $('#submit').click(() => {
+    let fromCurrency = $('.fromCurrency').val();
+    let toCurrency = $('.toCurrency').val();
+    makeApiCall(fromCurrency, toCurrency);
+  })
+}); 
