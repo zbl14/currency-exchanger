033be88405a696177f86933c2e1df6208a9fb53e
zbl14 <ifthereisoneday@gmail.com>
2022-07-01 11:55:25 -0700

    add dropdownlist for toCurrency

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './css/styles.css';
 import $ from 'jquery';
 import { ExchangeService } from './services/exchange-service.js';
 
+
+let exchangeList = (response) => {
+  if (response.conversion_rates) {
+    let currencyArr = Object.keys(response.conversion_rates)
+    let currencylist = ""
+    for (let i=1; i < currencyArr.length; i++) {
+      currencylist += `<option>${currencyArr[i]}</option>`;
+    }
+    $('#toCurrency').append(currencylist);
+  }
+}
+
+async function makeApiCallList(base) {
+  const response = await ExchangeService.getExchangeRate(base);
+  exchangeList (response);
+}
+
 let exchange = (response, toCurrency, amount) => {
   if (response.conversion_rates) {
     let toCurrencyAmount = `${response.conversion_rates[toCurrency]}` * amount;
@@ -21,9 +38,10 @@ async function makeApiCall(fromCurrency, toCurrency, amount) {
 }
 
 $(document).ready(function() {
+  makeApiCallList("USD")
   $('#submit').click(function() {
     let fromCurrency = $('.fromCurrency').val();
-    let toCurrency = $('.toCurrency').val();
+    let toCurrency = $('#toCurrency').val();
     let amount = $('#amount').val();
     makeApiCall(fromCurrency, toCurrency, amount);
   });
