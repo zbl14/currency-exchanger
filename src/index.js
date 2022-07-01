98017a6e8c774b76a6db174993a936bc57548748
zbl14 <ifthereisoneday@gmail.com>
2022-07-01 09:20:48 -0700

    WIP: async function for geting exchange rate

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+import { ExchangeService } from './services/exchange-service.js';
+
+let exchange = (from, to) => {
+  let to = to
+  if (from.conversion_rates) {
+    $(".toCurrency").val(`${from.conversion_rates.to}`)
+  }
+}
