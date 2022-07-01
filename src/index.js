152a65fcae0eb94ddfcdefda02699b8258fa8f1f
zbl14 <ifthereisoneday@gmail.com>
2022-07-01 13:28:19 -0700

    add error show for inputting currency that does not exist, clean console.log

--- src/index.js
+++ src/index.js
@@ -7,40 +7,44 @@ import { ExchangeService } from './services/exchange-service.js';
 
 let exchangeList = (response) => {
   if (response.conversion_rates) {
-    let currencyArr = Object.keys(response.conversion_rates)
-    let currencylist = ""
-    for (let i=1; i < currencyArr.length; i++) {
+    let currencyArr = Object.keys(response.conversion_rates);
+    let currencylist = "";
+    for (let i=0; i < currencyArr.length; i++) {
       currencylist += `<option>${currencyArr[i]}</option>`;
     }
+    $('#fromCurrency').append(currencylist);
     $('#toCurrency').append(currencylist);
+  } else {
+    $('.showErrors').show().text(`There was an error processing your request: ${response}`);
   }
-}
-
-async function makeApiCallList(base) {
-  const response = await ExchangeService.getExchangeRate(base);
-  exchangeList (response);
-}
+};
 
 let exchange = (response, toCurrency, amount) => {
   if (response.conversion_rates) {
     let toCurrencyAmount = `${response.conversion_rates[toCurrency]}` * amount;
     $(".fromCurrencyAmount").val(`${amount} ${response.base_code}`);
     $(".toCurrencyAmount").val(`${toCurrencyAmount} ${toCurrency}`);
+  } else if (response["error-type"]) {
+    $('.showErrors').show().text(`${response["error-type"]}. The currency in question doesn't exist.`);
   } else {
-    $('.showErrors').text(`There was an error processing your request: ${response}`);
+    $('.showErrors').show().text(`There was an error processing your request: ${response}`);
   }
 };
 
+async function makeApiCallList(query) {
+  const response = await ExchangeService.getExchangeRate(query);
+  exchangeList (response);
+}
+
 async function makeApiCall(fromCurrency, toCurrency, amount) {
   const response = await ExchangeService.getExchangeRate(fromCurrency);
-  console.log(response);
   exchange(response, toCurrency, amount);
 }
 
 $(document).ready(function() {
-  makeApiCallList("USD")
+  makeApiCallList("USD");
   $('#submit').click(function() {
-    let fromCurrency = $('.fromCurrency').val();
+    let fromCurrency = $('#fromCurrency').val();
     let toCurrency = $('#toCurrency').val();
     let amount = $('#amount').val();
     makeApiCall(fromCurrency, toCurrency, amount);
